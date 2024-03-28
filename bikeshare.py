9db76e5f944757c4abeea3259ea63a5843230588
Patrick Laurence <patrick.laurance@tui.co.uk>
2024-03-28 09:49:45 +0000

    updating comments for input_city

--- bikeshare.py
+++ bikeshare.py
@@ -14,7 +14,7 @@ def get_filters():
     Returns:
         city, month and day filters
     """
-    #Filter by city
+    #If input_city exists or = 'all' assign to city
     while True:
         input_city = input("What city would you like to view? Chicago, New York City, Washington or all?  \n").lower() 
         if input_city in CITY_DATA.keys() or input_city == 'all':
